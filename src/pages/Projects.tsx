37bfb9a7adfc5ec325e20c7dd533b807959935cd
ralphiedawg <russellwpage@gmail.com>
2025-01-07 13:02:28 -0500

    add bitty

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -11,12 +11,22 @@ const Projects = () => {
             <p className="mt-4 text-4xl text-customDark">Here are some of the projects I've worked on:</p>
             <div className="mt-8 space-y-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 <div className="relative bg-white shadow-lg rounded-lg overflow-hidden">
-                   <img src={schedulespx} alt="Project Preview" className="w-full h-auto" />
+                    <img src={schedulespx} alt="Project Preview" className="w-full h-auto" />
                     <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center p-4">
                         <BlurFade className="text-white">
                             <h2 className="text-2xl font-semibold">ScheduleSPX</h2>
                             <p className="mt-2 text-gray-200">The All-in-One Student Dashboard</p>
                             <a href="https://schedulespx.com/about" className="mt-4 inline-block text-blue-300 hover:underline"> View Project </a>
+                        </BlurFade>
+                    </div>
+                </div>
+                <div className="relative bg-white shadow-lg rounded-lg overflow-hidden">
+                    {/* <img src={REPLACEME} alt="Project Preview" className="w-full h-auto" /> */}
+                    <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center p-4">
+                        <BlurFade className="text-white">
+                            <h2 className="text-2xl font-semibold">Bitty</h2>
+                            <p className="mt-2 text-gray-200">An Open-Source Bittorrent Client</p>
+                            <a href="https://github.com/ralphiedawg/bitty" className="mt-4 inline-block text-blue-300 hover:underline"> View Project </a>
                         </BlurFade>
                     </div>
                 </div>
